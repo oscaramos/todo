71137b5507be5d8a7830b831f796619abbb35698
Oscar <oscar.ramos@ucsp.edu.pe>
2020-08-25 01:25:58 -0500

    Customized Radio Buttoms for checkmark

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -5,6 +5,7 @@ import Grid from '@material-ui/core/Grid'
 import { Typography } from '@material-ui/core'
 import Paper from '@material-ui/core/Paper'
 import Radio from '@material-ui/core/Radio'
+import clsx from 'clsx'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,6 +16,41 @@ const useStyles = makeStyles(theme => ({
 const useTasksStyles = makeStyles(theme => ({
   paper: {
     height: '5em',
+  },
+  icon: {
+    'borderRadius': '50%',
+    'width': 16,
+    'height': 16,
+    'boxShadow': 'inset 0 0 0 1px rgba(16, 22, 26, 0.2), inset 0 -1px 0 rgba(16, 22, 26, 0.1)',
+    'backgroundColor': '#f5f8fa',
+    'backgroundImage': 'linear-gradient(180deg, hsla(0, 0%, 100%, 0.8), hsla(0, 0%, 100%, 0))',
+
+    '&:hover ~ &': {
+      backgroundColor: '#74BE4F',
+      backgroundImage: 'linear-gradient(180deg, hsla(0, 0%, 100%, 0.1), hsla(0, 0%, 100%, 0))',
+    },
+
+    '&:hover ~ &:before': {
+      display: 'block',
+      width: 16,
+      height: 16,
+      backgroundImage: `url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23aaa'/%3E%3C/svg%3E")`,
+      content: "''",
+    },
+  },
+  checkedIcon: {
+    backgroundColor: '#91DC5A',
+    backgroundImage: 'linear-gradient(180deg, hsla(0, 0%, 100%, 0.1), hsla(0, 0%, 100%, 0))',
+    '&:before': {
+      display: 'block',
+      width: 16,
+      height: 16,
+      backgroundImage: `url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E")`,
+      content: '""',
+    },
+    'input:hover ~ &': {
+      backgroundColor: '#74BE4F',
+    },
   },
 }))
 
@@ -67,6 +103,9 @@ function ViewTasks({ tasks, toggleCompleted }) {
                           <Radio
                             checked={task.completed}
                             onClick={() => toggleCompleted(index)}
+                            icon={<span className={classes.icon} />}
+                            checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
+                            disableRipple
                           />
                         </Grid>
                         <Grid item sm={3}>
