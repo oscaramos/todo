596771ed4ab8c5767ddb568689241855904efa95
Oscar <oscar.ramos@ucsp.edu.pe>
2020-08-26 23:02:33 -0500

    Add the Add Task Button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
+
 import Paper from '@material-ui/core/Paper'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+import Fab from '@material-ui/core/Fab'
+import Grid from '@material-ui/core/Grid'
 
+import AddIcon from '@material-ui/icons/Add'
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined'
 import AssignmentOutlinedIcon from '@material-ui/icons/AssignmentOutlined'
 
@@ -14,18 +18,26 @@ const Footer = () => {
   };
 
   return (
-    <Paper square>
+    <Paper square style={{ position: 'relative', overflow: 'visible' }}>
       <Tabs
         value={value}
         onChange={handleChange}
         variant='fullWidth'
         indicatorColor='secondary'
         textColor='secondary'
-        aria-label='icon label tabs example'
+        aria-label='footer label tabs'
       >
         <Tab icon={<HomeOutlinedIcon />} label='Home' />
         <Tab icon={<AssignmentOutlinedIcon />} label='Task' />
       </Tabs>
+
+      <Grid container style={{ position: 'absolute', top: -25 }} justify='center'>
+        <Grid item>
+          <Fab color='primary' aria-label='add task'>
+            <AddIcon fontSize='large' />
+          </Fab>
+        </Grid>
+      </Grid>
     </Paper>
   )
 }
